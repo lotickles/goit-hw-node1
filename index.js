16d1bf42b257e04391dd046db8cae19ec83ab7ab
lotickles <148078790+lotickles@users.noreply.github.com>
2024-09-22 07:03:21 +0800

    tried the demos 1-5

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-console.log;
-("hello Lot");
+// console.log("hello Lot");
+//-------
+//demo2 global variable
+// globalThis.foo = 5;
+// console.log(global.foo);
+//-----
+//demo3 Common Js Module
+// const name = "Lotlot";
+// const age = 40;
+// const color = "red";
+// module.exports = { name, age, color };
+
+//demo4 converting into ESM, in package.json put before "script" this   "type": "module",
+// const color = "red";
+// const age = 40;
+// const name = "Lotlot";
+// export { color, age, name };
+
+//demo 5 working with files ex, convert thi into esm const fs = require("fs/promises")
+//FS =file system
+// fs.readFile(filename, [options]) - read file
+// fs.writeFile(filename, data, [options]) - write file
+// fs.appendFile(filename, data, [options])- append to file
+// fs.rename(oldPath, newPath) - rename file
+// fs.unlink(path, callback) - delete file
+
+import fs from "fs/promises";
+//how to read file
+fs.readFile("readme.txt")
+  .then((data) => console.log(data.toString()))
+  .catch((err) => console.log(err.message));
+
+// //how to write file
+// fs.writeFile("readme.txt", "Hello, I'm editing this txt via writeFile()");
+//append file
+fs.appendFile("readme.txt", "\nThis is appended text");
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Hello, I'm editing this txt via writeFile()
+This is appended text
+
+This is appended text
+This is appended text
